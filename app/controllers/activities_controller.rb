95209883ffcc1a90d244d316abe0277eeee26d22
Claire Tran <c.tran83@gmail.com>
2014-11-29 15:36:53 +0000

    adding an activity into the DB

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,16 @@
 class ActivitiesController < ApplicationController
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.save
+    redirect_to @activity
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+private
+  def activity_params
+    params.require(:activity).permit(:name, :location, :time, :creator_id)
+  end
 end
